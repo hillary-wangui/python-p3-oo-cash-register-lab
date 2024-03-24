45c9801e95d484419bf2eaf5fc8b7e5b7e9d54af
hillary-wangui <145745223+hillary-wangui@users.noreply.github.com>
2024-03-24 20:10:40 +0300

    Update cash_register.py

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,30 @@
-#!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self):
+        self.total = 0
+        self.items = []
+        self.last_transaction_amount = 0
+
+    def add_item(self, item_name, price, quantity=1):
+        if quantity <= 0:
+            return "Invalid quantity value"
+        for _ in range(quantity):
+            self.items.append(item_name)
+            self.total += price
+            self.last_transaction_amount = price
+        return self.total
+
+    def void_last_transaction(self):
+        if self.last_transaction_amount != 0:
+            self.total -= self.last_transaction_amount
+            self.last_transaction_amount = 0
+            return self.total
+        else:
+            return "No transaction to void"
+
+    def apply_discount(self, discount):
+        if discount > 0 and discount <= 100:
+            discount_amount = self.total * discount / 100
+            self.total -= discount_amount
+            return self.total
+        else:
+            return "Invalid discount value"
